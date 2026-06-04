19e9d5795a734fc941b5bacb19a6b7a065697411
nithyan <nithyancr7.0@gmail.com>
2026-06-04 13:27:13 +0530

    Create product.js

---
+++ product.js
@@ -0,0 +1,26 @@
+const products = [
+{
+id:1,
+name:"Laptop",
+price:55000,
+image:"https://picsum.photos/250?1"
+},
+{
+id:2,
+name:"Mobile",
+price:25000,
+image:"https://picsum.photos/250?2"
+},
+{
+id:3,
+name:"Headphones",
+price:3000,
+image:"https://picsum.photos/250?3"
+},
+{
+id:4,
+name:"Smart Watch",
+price:5000,
+image:"https://picsum.photos/250?4"
+}
+];
